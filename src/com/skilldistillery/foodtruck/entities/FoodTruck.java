31cb036a878fbd6da34f9c720c7d4ed2c5835e9e
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-03-25 16:46:39 -0600

    added FT name

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,7 +2,7 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 	private static int nextTruckId;
-	private name
+	private String name;
 	private foot type
 	private numeric rating
 
